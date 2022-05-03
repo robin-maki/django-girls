c29728b02b8ab6846c35dddf7fb78e1065053e88
robin* <robin_maki@planet.moe>
2022-05-03 12:55:29 +0900

    실습과제 06

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def html5(req):
+    return render(req, 'blog/html5.html')
